a9dbf304de85f8e59a01b85acc113b86fde8e09b
Gokul <91804028+bi66y@users.noreply.github.com>
2023-03-17 00:02:16 +0530

    Update sub-miner.sh

--- sub-miner.sh
+++ sub-miner.sh
@@ -19,11 +19,19 @@ subfinder -d $domain -o ./subs/subf.txt
 assetfinder $domain | tee -a ./subs/assf.txt
 sublist3r -d $domain -o ./subs/subl.txt
 
+echo "Sorting.."
 cat ./subs/*.txt | anew ./allsubs.txt
+echo "Live Domains"
 httpx -l allsubs.txt --silent -o live.txt
+echo "CNAMES"
 httpx -l live.txt -mc 404 -cname -o cnames.txt
+echo "Content Length"
 httpx -l live.txt --content-length -mc 200 -o content.txt
+echo "IPs"
 cat live.txt | dnsx -a --resp-only -o ip.txt
+echo "NRICH saved"
 cat ip.txt | nrich - >> nrich.txt
+echo "Status Code and Titles"
 httpx -l live.txt -p 80,443,8080,3000 -status-code -title -o titles.txt
+echo "Code 403"
 httpx -l live.txt --silent -title -mc 403 -o forbidden.txt
